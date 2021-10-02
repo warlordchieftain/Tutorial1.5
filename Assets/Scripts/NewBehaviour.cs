becdaffb4c8b4c77586b97c4a42a6678de092019
warlordchieftain <jgaleassi@gmail.com>
2021-10-01 21:38:02 -0400

    Animation commit

---
+++ Assets/Scripts/NewBehaviour.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviour : MonoBehaviour
+{
+
+    public AudioClip musicClipOne;
+
+public AudioClip musicClipTwo;
+
+public AudioSource musicSource;
+
+Animator anim; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+       if (Input.GetKey("escape"))
+      {
+            Application.Quit();
+      }
+    
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+          musicSource.clip = musicClipOne;
+          musicSource.Play();
+          anim.SetInteger("State", 1);
+        }
+        
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+        }
+          if (Input.GetKeyDown(KeyCode.R))
+        {
+          musicSource.clip = musicClipTwo;
+          musicSource.Play();
+          anim.SetInteger("State", 2);
+        }
+        
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+        }
+    
+     if (Input.GetKeyDown(KeyCode.L))
+        {
+          musicSource.loop = true;
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+        {
+          musicSource.loop = false;
+        }
+   }
+} 
